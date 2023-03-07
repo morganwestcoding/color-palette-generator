00010c98d3705ac08233b48a1f2abd6e56cbabc1
Morgan West <mewwes@gmail.com>
2023-03-06 17:58:56 -0700

    final responsizeness

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const refreshBtn = document.querySelector(".refresh-btn");
 const maxPaletteBoxes = 32;
 
 const generatePalette = () => {
+    container.innerHTML = "";
     for (let i = 0; i < maxPaletteBoxes; i++) {
         
     let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
@@ -13,8 +14,19 @@ const generatePalette = () => {
     color.classList.add("color");
     color.innerHTML = `<div class="rect-box" style="background: ${randomHex}"></div>
                        <span class="hex-value">${randomHex}</span>`;
+    color.addEventListener("click", () => copyColor(color, randomHex));
     container.appendChild(color);
     }
 }
+
+generatePalette();
+
+const copyColor = (elem, hexVal) => {
+    const colorElement = elem.querySelector(".hex-value");
+    navigator.clipboard.writeText(hexVal).then(() => {
+        colorElement.innerText = "Copied";
+        setTimeout(() => colorElement.innerText = hexVal, 1000);
+    })
+};
 
 refreshBtn.addEventListener("click", generatePalette);
